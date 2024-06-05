dac08278e255e04ae25256afc07006134b95026c
chayanbajaj <45946285+chayanbajaj@users.noreply.github.com>
2024-06-05 19:07:27 +0530

    adds calculator logic

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,7 +1,25 @@
 function add(numbers: string): number {
+  let delim;
+  const [delims, ...numberStrings] = numbers.split("\n").map((s) => s.trim());
+  if (delims.startsWith("//")) {
+    delim = delims.slice(2);
+    const delimiter = delim.charAt(0);
+    numbers = numberStrings
+      .join("\n")
+      .replace(new RegExp(`\\${delimiter}`, "g"), ",");
+  }
 
-    return 0;
+  const numbersArray = numbers.split(",").map(Number);
+
+  if (numbersArray.some((num) => num < 0)) {
+    throw new Error(
+      `negative numbers not allowed ${numbersArray
+        .filter((num) => num < 0)
+        .join(",")}`
+    );
+  }
+
+  return numbersArray.reduce((acc, curr) => acc + curr, 0);
 }
-  
-  export { add };
-  
+
+export { add };
